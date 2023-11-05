c2b7b88ab15e70acfeb59eef204ad6b4cd61bdbb
SirLevid <jassontejada@gmail.com>
2023-11-04 21:12:04 -0400

    Using LazyVerticalGrid

--- app/src/main/java/com/levid/random_users/ui/user/UserScreen.kt
+++ app/src/main/java/com/levid/random_users/ui/user/UserScreen.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -44,7 +43,7 @@ fun UserScreen(
 
 @Composable
 fun DisplayList(users: List<UserDto>) {
-    LazyColumn {
+    LazyVerticalGrid(GridCells.Fixed(2)) {
         items(users) { user ->
             UserItem(user)
         }
@@ -79,7 +78,7 @@ fun UserItem(user: UserDto) {
                         shape = CircleShape
                     )
                     .clip(CircleShape)
-                    .aspectRatio(1f,true)
+                    .aspectRatio(1f, true)
             )
         }
 
